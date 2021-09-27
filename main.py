50dac83274a01348494bc71ad5697220b359635a
Foxe4k <foxeivan2007@gmail.com>
2021-09-27 18:57:29 +0300

    Русский алфавит

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET_EN = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET_RU ="абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+
+language=input("Введите язык(RU-Русский;EN-Английский)")
 MessageToEncrypt = input("Введите строку для шифра")
-encryptedMessege=""
+encryptedMessege = ""
+if language == "RU":
+    ALPHABET=ALPHABET_RU
+if language == "EN":
+    ALPHABET=ALPHABET_EN
 for letter in MessageToEncrypt:
     place = ALPHABET.find(letter)
     newPlace = (place + 1 + len(ALPHABET)) % len(ALPHABET)
     if letter in ALPHABET:
-        encryptedMessege+=ALPHABET[newPlace]
+        encryptedMessege += ALPHABET[newPlace]
     else:
-        encryptedMessege+=letter
+        encryptedMessege += letter
 print(encryptedMessege)
