c60e50f771590572202e207d6619b5fd8d81da21
Foxe4k <foxeivan2007@gmail.com>
2021-09-27 18:31:04 +0300

    Испр. Багов

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ message=input("Введите строку для шифра")
 encryptedMessege=""
 for i in message:
     place = alf.find(i)
-    newPlace =place
+    newPlace =(place + 1 + len(alf)) % len(alf)
     if i in alf:
         encryptedMessege+=alf[newPlace]
     else:
